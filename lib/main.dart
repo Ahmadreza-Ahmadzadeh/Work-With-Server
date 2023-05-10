f0cdefbc90f94f868f070eb217cc74cf8828277c
Ahmad <ahmad.n.e.m.93@gmail.com>
2023-05-10 22:11:47 +0330

    Add studentForm screen & use feature post from DIO

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,36 +15,53 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Home Page',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          inputDecorationTheme:
+              InputDecorationTheme(border: OutlineInputBorder()),
+          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+              .copyWith(secondary: Color(0xff16E5A7))),
       home: const HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Android Expert'),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () async{
+            final result=await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => _AddStudentForm(),
+                
+              ),
+              
+            );
+            setState(() {
+              
+            });
+          },
+          label: Row(
+            children: [Icon(Icons.add), Text('Add Student')],
+          )),
       body: FutureBuilder<List<StudentData>>(
         future: getStudents(),
         builder: (context, snapshot) {
           if (snapshot.hasData && snapshot.hasData != null) {
             return ListView.builder(
+              padding: EdgeInsets.only(bottom: 84),
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 return _Student(data: snapshot.data![index]);
@@ -57,6 +73,87 @@ class HomeScreen extends StatelessWidget {
             );
           }
         },
+      ),
+    );
+  }
+}
+
+class _AddStudentForm extends StatelessWidget {
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _courseController = TextEditingController();
+  final TextEditingController _scoreController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add New Student'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          try {
+            final newStudentData=await saveStudent(
+              _firstNameController.text,
+              _lastNameController.text,
+              _courseController.text,
+              int.parse(_scoreController.text),
+            );
+            Navigator.pop(context,newStudentData);
+          } catch (e) {
+            debugPrint(e.toString());
+          }
+        },
+        label: Row(
+          children: [
+            Icon(Icons.check),
+            Text('Save'),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _firstNameController,
+              decoration: InputDecoration(
+                label: Text('First Name'),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: _lastNameController,
+              decoration: InputDecoration(
+                label: Text('Last Name'),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: _courseController,
+              decoration: InputDecoration(
+                label: Text('Course'),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              controller: _scoreController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                label: Text('Score'),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+          ],
+        ),
       ),
     );
   }
